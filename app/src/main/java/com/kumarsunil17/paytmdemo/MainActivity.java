354713fbeb5b8531cbddca90f8c25d190ea9da93
KumarSunil17 <42208638+KumarSunil17@users.noreply.github.com>
2019-06-06 17:47:45 +0530

    Initial commit

---
+++ app/src/main/java/com/kumarsunil17/paytmdemo/MainActivity.java
@@ -0,0 +1,40 @@
+package com.kumarsunil17.paytmdemo;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText amount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+
+        amount = findViewById(R.id.amount);
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_SMS, Manifest.permission.RECEIVE_SMS}, 101);
+        }
+    }
+
+    public void proceedToPay(View view) {
+        if (!TextUtils.isEmpty(amount.getText().toString())) {
+
+            Intent i = new Intent(MainActivity.this, PaytmActivity.class);
+            i.putExtra("rupees", amount.getText().toString());
+            startActivity(i);
+        }
+    }
+}
